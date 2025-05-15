591cefda7ad6e4b1d5444e56d21a7fdce6a40d93
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-05-15 19:57:59 +0200

    docs : comment backToTopPage

--- front/src/components/BackToTopPage.tsx
+++ front/src/components/BackToTopPage.tsx
@@ -2,6 +2,7 @@ import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
 export default function BackToTopPage() {
+  // Scrolls to the top of the page whenever the URL/path changes
   const location = useLocation();
 
   useEffect(() => {
