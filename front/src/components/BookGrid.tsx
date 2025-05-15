4769d9b1771ffbae4146c97c52e97250a88d4e44
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-05-15 20:25:59 +0200

    refactor : remove popular book component

--- front/src/components/BookGrid.tsx
+++ front/src/components/BookGrid.tsx
@@ -24,16 +24,16 @@ const BookGrid = ({ title, fetchBooks }: BookGridProps) => {
     <section className="content ml-[5vw] mr-[5vw] pt-8">
       <h2 className="text-3xl mb-4 font-bold font-title">{title}</h2>
       <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 lg:grid-cols-5">
-        {bookList.map((popularBook) => {
+        {bookList.map((book) => {
           return (
-            <Link key={popularBook.id} to={`/books/${popularBook.id}`} className="block">
+            <Link key={book.id} to={`/books/${book.id}`} className="block">
               <div className="book cursor-pointer hover:shadow-lg hover:rounded-md hover:transition-shadow">
                 <img
-                  src={`https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/${popularBook.cover_url}.jpg`}
-                  alt={popularBook.title}
+                  src={`https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/${book.cover_url}.jpg`}
+                  alt={book.title}
                   className="h-80 w-full object-contain mb-2 mx-auto"
                 />
-                <p className="text-center text-lg font-body [word-spacing:2px] tracking-wider ">{popularBook.title}</p>
+                <p className="text-center text-lg font-body [word-spacing:2px] tracking-wider ">{book.title}</p>
               </div>
             </Link>
           );
