e32e314e4abaf13af4ca41fe590b32a3871e7449
Guilhem-allegre <guilhem.allegre@oclock.school>
2025-05-16 11:40:01 +0200

    feat: seo in SearchPage file

--- front/src/pages/SearchPage.tsx
+++ front/src/pages/SearchPage.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { useLocation, Link } from "react-router-dom";
 import { searchBooks } from "../api/apiBooks";
 import { IBook } from "../@types";
+import Seo from "../components/Seo";
+
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 //get query string
 const useQuery = () => {
@@ -25,32 +28,38 @@ const SearchPage = () => {
   }, [searchTerm]); // rerun when searchTerm changes
 
   return (
-    <div>
-     
-      <section className="content ml-[5vw] mr-[5vw]">
-        <h2 className="text-xl mt-8 mb-4 font-bold">
-          {results.length} Résultat(s) pour la recherche : " {searchTerm} "
-        </h2>
-        {results.length > 0 ? (
-          <div className="book-list grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-5">
-            {results.map((book: any) => (
-              <Link key={book.id} to={`/books/${book.id}`} className="block">
-                <div className="book cursor-pointer hover:shadow-lg hover:rounded-md hover:transition-shadow">
-                  <img
-                    src={`https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/${book.cover_url}.jpg`}
-                    alt={book.title}
-                    className="h-80 w-full object-contain mb-2 mx-auto"
-                  />
-                  <p className="text-center">{book.title}</p>
-                </div>
-              </Link>
-            ))}
-          </div>
-        ) : (
-          <p>Aucun résultat trouvé.</p>
-        )}
-      </section>
-    </div>
+    <>
+       <Seo
+        title="Recherche"
+        description="Résultat de votre recherche"
+        url={`${baseUrl}/search`}
+      />
+      <div>
+        <section className="content ml-[5vw] mr-[5vw]">
+          <h2 className="text-xl mt-8 mb-4 font-bold">
+            {results.length} Résultat(s) pour la recherche : " {searchTerm} "
+          </h2>
+          {results.length > 0 ? (
+            <div className="book-list grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-5">
+              {results.map((book: any) => (
+                <Link key={book.id} to={`/books/${book.id}`} className="block">
+                  <div className="book cursor-pointer hover:shadow-lg hover:rounded-md hover:transition-shadow">
+                    <img
+                      src={`https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/${book.cover_url}.jpg`}
+                      alt={book.title}
+                      className="h-80 w-full object-contain mb-2 mx-auto"
+                    />
+                    <p className="text-center">{book.title}</p>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          ) : (
+            <p>Aucun résultat trouvé.</p>
+          )}
+        </section>
+      </div>
+    </>
   );
 };
 
