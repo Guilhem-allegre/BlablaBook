1ad06a9184f9a91aea813586398f683ef11f5baf
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-05-19 14:56:06 +0200

    feat : display decimal numbers

--- front/src/pages/CategoryPage.tsx
+++ front/src/pages/CategoryPage.tsx
@@ -4,6 +4,7 @@ import { IBook } from "../@types";
 import { useErrorHandler } from "../utils/useErrorHandler";
 import { getBooksByCategory } from "../api/apiBooks";
 import Seo from "../components/Seo";
+import StarRatingStatic from "../components/review/StarRating";
 
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
@@ -27,11 +28,7 @@ export default function CategoryPage() {
 
   return (
     <>
-       <Seo
-        title="Catégories"
-        description="Découvrez nos livres par catégories"
-        url={`${baseUrl}/categories/${categoryId}`}
-      />
+      <Seo title="Catégories" description="Découvrez nos livres par catégories" url={`${baseUrl}/categories/${categoryId}`} />
       <div className="bg-body font-sans pt-8">
         {/* Contenu principal avec marge à gauche */}
 
@@ -39,12 +36,9 @@ export default function CategoryPage() {
         <section className="content">
           <div>
             <div className="bg-nav-footer-50 font-sans">
-              {/* Contenu principal avec marge à gauche */}{" "}
-              {/* Ajoute une marge à gauche sur les écrans md et plus grands */}
+              {/* Contenu principal avec marge à gauche */} {/* Ajoute une marge à gauche sur les écrans md et plus grands */}
               <section className="content ml-[5vw] mr-[5vw] pb-20 ">
-                <h2 className="text-2xl mb-4 font-title font-bold">
-                  Tous Nos Livres du genre : {booksCategories[0]?.categories?.[0]?.name}
-                </h2>
+                <h2 className="text-2xl mb-4 font-title font-bold">Tous Nos Livres du genre : {booksCategories[0]?.categories?.[0]?.name}</h2>
                 <div className="book-list grid grid-cols-2 gap-x-6 gap-y-8 sm:grid-cols-3 lg:grid-cols-5">
                   {/* loop on books */}
                   {booksCategories.map((book) => (
@@ -57,6 +51,13 @@ export default function CategoryPage() {
                           className="h-80 w-100 object-contain mb-2 mx-auto"
                         />
                         <p className="font-body tracking-wider [word-spacing:2px] text-lg">{book.title}</p>
+                      </div>
+                      <div className="mt-2 min-h-[28px] flex justify-center">
+                        {book.averageRating !== null ? (
+                          <StarRatingStatic rating={Number(book.averageRating)} showValue />
+                        ) : (
+                          <p className="text-sm text-gray-500 italic">Pas encore noté</p>
+                        )}
                       </div>
                     </Link>
                   ))}
