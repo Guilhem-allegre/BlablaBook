87502eec037e3c58569bc9703716d28dc17947c1
AmndineBlt <amandine.bluteau@oclock.school>
2025-05-19 09:37:01 +0200

    update: review validate schema

--- back/src/middlewares/schemaValidate/reviewValidateSchema.js
+++ back/src/middlewares/schemaValidate/reviewValidateSchema.js
@@ -1,9 +1,20 @@
 import Joi from "joi";
 
 const reviewSchema = Joi.object({
-  rating: Joi.number().integer().min(0).max(5),
-  title: Joi.string().max(100),
-  comment: Joi.string().max(1000),
+  rating: Joi.number().integer().min(0).max(5).messages({
+    "number.base": `"rating" doit être un nombre`,
+    "number.integer": `"rating" doit être un entier`,
+    "number.min": `"rating" doit être au moins {#limit}`,
+    "number.max": `"rating" doit être au plus {#limit}`,
+  }),
+  title: Joi.string().max(100).messages({
+    "string.base": `"title" doit être une chaîne de caractères`,
+    "string.max": `"title" ne doit pas dépasser {#limit} caractères`,
+  }),
+  comment: Joi.string().max(1000).messages({
+    "string.base": `"comment" doit être une chaîne de caractères`,
+    "string.max": `"comment" ne doit pas dépasser {#limit} caractères`,
+  }),
 });
 
 export { reviewSchema };
