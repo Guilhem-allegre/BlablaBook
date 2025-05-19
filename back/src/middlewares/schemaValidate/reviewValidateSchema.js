8c7973bec97844e01d4887aa4617ff1668de6975
AmndineBlt <amandine.bluteau@oclock.school>
2025-05-19 09:37:55 +0200

    update: review validate schema

--- back/src/middlewares/schemaValidate/reviewValidateSchema.js
+++ back/src/middlewares/schemaValidate/reviewValidateSchema.js
@@ -2,18 +2,18 @@ import Joi from "joi";
 
 const reviewSchema = Joi.object({
   rating: Joi.number().integer().min(0).max(5).messages({
-    "number.base": `"rating" doit être un nombre`,
-    "number.integer": `"rating" doit être un entier`,
-    "number.min": `"rating" doit être au moins {#limit}`,
-    "number.max": `"rating" doit être au plus {#limit}`,
+    "number.base": `La note doit être un nombre`,
+    "number.integer": `La note doit être un entier`,
+    "number.min": `La note doit être au moins {#limit}`,
+    "number.max": `La note doit être au plus {#limit}`,
   }),
   title: Joi.string().max(100).messages({
-    "string.base": `"title" doit être une chaîne de caractères`,
-    "string.max": `"title" ne doit pas dépasser {#limit} caractères`,
+    "string.base": `Le titre doit être une chaîne de caractères`,
+    "string.max": `Le titre ne doit pas dépasser {#limit} caractères`,
   }),
   comment: Joi.string().max(1000).messages({
-    "string.base": `"comment" doit être une chaîne de caractères`,
-    "string.max": `"comment" ne doit pas dépasser {#limit} caractères`,
+    "string.base": `Le commentaire doit être une chaîne de caractères`,
+    "string.max": `Le commentaire ne doit pas dépasser {#limit} caractères`,
   }),
 });
 
